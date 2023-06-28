56268d0cb675f91471597a5e0ba87645f32a2cc9
Al Elliott <adrelliott@gmail.com>
2023-06-28 20:01:21 +0200

    Create Invitation Action

---
+++ app/Actions/Invitations/InviteContactsToSurvey.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions\Invitations;
+
+use App\Models\Survey;
+use Carbon\Carbon;
+
+class InviteContactsToSurvey
+{
+    public function handle(Survey $survey, mixed $contactIds, string $invitedAt = null): void
+    {
+        $survey->contacts()->syncWithPivotValues(
+            is_array($contactIds) ? $contactIds : [$contactIds],
+            ['invited_at' => $invitedAt ?: now(),]
+        );
+    }
+}
---
+++ tests/Feature/InvitationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Actions\Invitations\InviteContactsToSurvey;
+use App\Models\Survey;
+use App\Models\Contact;
+use App\Models\Organisation;
+use Carbon\Carbon;
+
+beforeEach(function() {
+    $this->organisation = Organisation::factory()
+        ->has(Contact::factory(10))
+        ->has(Survey::factory(1))
+        ->create();
+});
+
+test('can invite single contact to a survey', function () {
+    app(InviteContactsToSurvey::class)->handle(
+        $this->organisation->surveys->first(), 
+        $this->organisation->contacts->first()->id
+    );
+
+    $survey = Survey::firstOrFail();
+    $this->assertCount(1, $survey->contacts);
+    $this->expect($survey->contacts->first()->id)
+        ->toBe($this->organisation->contacts->first()->id);
+});
+
+test('can invite multiple contacts to a survey', function () {
+    app(InviteContactsToSurvey::class)->handle(
+        $this->organisation->surveys->first(), 
+        $this->organisation->contacts->take(5)->pluck('id')->toArray()
+    );
+
+    $survey = Survey::firstOrFail();
+    $this->assertCount(5, $survey->contacts);
+    $this->expect($survey->contacts->first()->id)
+        ->toBe($this->organisation->contacts->first()->id);
+});
+
+test('can invite contacts to survey but with a future date', function () {
+    $inviteDate = now()->addDays(5)->format('Y-m-d H:i:s');
+    app(InviteContactsToSurvey::class)->handle(
+        $this->organisation->surveys->first(), 
+        $this->organisation->contacts->take(5)->pluck('id')->toArray(),
+        $inviteDate
+    );
+
+    $survey = Survey::firstOrFail();
+    $this->assertCount(5, $survey->contacts);
+    $this->expect($survey->contacts->first()->id)
+        ->toBe($this->organisation->contacts->first()->id);
+    $this->expect($survey->contacts->first()->pivot->invited_at)
+        ->toBe($inviteDate);
+});
